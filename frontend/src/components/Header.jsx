ac801cbaab488b86e2d894dd641534c2c598ad6e
daniel <ngan.yiching@gmail.com>
2024-01-15 16:17:53 -0500

    i just commit that all

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,33 @@
+import { Navbar, Nav, Container} from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import logo from '../assets/logo.png'
+import { LinkContainer } from 'react-router-bootstrap';
+
+const Header = () => {
+    return (
+        <header>
+            <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+                <Container>
+                    <Navbar.Brand href="/">
+                        <img src={logo} alt='ProShop' />
+                        ProShop</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-nvabar-nav">
+                        <Nav className="ms-auto">
+                            <LinkContainer to='/cart'>
+                                <Nav.Link href="/cart"><FaShoppingCart /> Cart</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/login'>
+                                <Nav.Link href='/login'>
+                                    <FaUser />Sign In
+                                </Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+
+            </Navbar>
+        </header>
+    )
+}
+export default Header
---
+++ frontend/src/components/Rating.jsx
@@ -0,0 +1,8 @@
+import {FaStar, FaStarHalfAlt, FaRegStar} from 'react-icons/fa'
+
+const Rating = () => {
+    return (
+        <div className='rating'>Rating</div>
+    )
+}
+export default Rating
