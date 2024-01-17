e29db122ca250c3aca3354990d47916f960d2046
daniel <ngan.yiching@gmail.com>
2024-01-17 09:23:45 -0500

    my last commit

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,32 +1,18 @@
-import {useEffect, useState} from 'react';
 import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap';
 import Product from '../components/Product';
 import Rating from '../components/Rating';
-import axios from 'axios';
+
 
 const HomeScreen = () => {
-    const {data: products, isLoading, error} = useGetProductsQuery();
+    const [products, setProducts] = useState([]);
 
-    return (
-        <>
-        {isLoading ? (
-          <div>Loading...</div>
-        ) : error ? (
-          <div>{error?.data.message || error.error}</div>
-        ) : (
-          <>
-            <h1>Latest Products</h1>
-            <Row>
-              {products.map((product) => (
-                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                  <Product product={product} />
-                </Col>
-              ))}
-            </Row>
-          </>
-        )}
-      </>
-    )
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const { data } = await axios.get('/api/products');
+            setProducts(data);
+        };
+        fetchProducts();
+    }, [])
 }
 
 export default HomeScreen
