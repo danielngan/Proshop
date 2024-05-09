a1f682ad5ac5db746bce507a2a4019d74e46c002
Daniel Ngan <ngan.yiching@gmail.com>
2024-05-09 16:51:56 -0400

    Update cartSlice.js
    change the cart slice file

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -19,6 +19,10 @@ const cartSlice = createSlice({
             } else {
                 state.cartItems = [...state.cartItems, item];
             }
+
+            state.itemsPrice = addDecimals(state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0));
+
+            state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10)
             return updateCart(state);
 
         },
